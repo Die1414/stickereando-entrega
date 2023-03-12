f62fa26ace90b6f7e86b4d634b01818a0000f77b
Diego1414 <dcanseco.89@hotmail.com>
2023-03-12 20:26:38 -0300

    se agrega suma de precios e iva

--- js/app.js
+++ js/app.js
@@ -2,21 +2,24 @@
 
 
 let cantidad = 0;
+let totalCompra = 0;
 
 while (true) {
   console.log(`Cantidad actual de Remeras: ${cantidad}`);
-
+  
   let respuesta = prompt(`Desea sumar un producto más? En caso de necesitar sumar un producto coloque "+" en caso de querer quitar un producto coloque "-" . Para finalizar la compra escriba "fin".`);
 
   if (respuesta === "+") {
     if (cantidad < 9) {
       cantidad++;
+      totalCompra += 300; // se suma $300 por cada producto agregado
     } else {
       alert("Para cantidades mayores a 10 le sugerimos ir a la sección de packs.");
     }
   } else if (respuesta === "-") {
     if (cantidad > 0) {
       cantidad--;
+      totalCompra -= 300; // se resta $300 por cada producto quitado
     } else {
       console.log("No puede quitar más productos, ya que no hay ninguno en el carrito.");
     }
@@ -27,3 +30,10 @@ while (true) {
     console.log("Respuesta no válida. Por favor ingrese un signo +, - o la palabra 'fin'.");
   }
 }
+
+// Calculamos el impuesto y mostramos el total final
+const impuesto = totalCompra * 0.21;
+const totalConImpuesto = totalCompra + impuesto;
+console.log(`Total de la compra: $${totalCompra}`);
+console.log(`Impuesto del 21%: $${impuesto}`);
+console.log(`Total con impuesto: $${totalConImpuesto}`);
